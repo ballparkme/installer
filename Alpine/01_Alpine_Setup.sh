5c7db9088a21bb3df9d07b4e63b68c8003ddd016
AllinAll <40621866+ballparkme@users.noreply.github.com>
2026-05-21 15:14:28 +0800

    Modify APK setup and include bash in installations
    Updated APK repository setup and added bash to installed packages.

--- Alpine/01_Alpine_Setup.sh
+++ Alpine/01_Alpine_Setup.sh
@@ -9,12 +9,12 @@ echo "[1/4] 正在设置时区为 上海 (Asia/Shanghai)..."
 setup-timezone -z Asia/Shanghai
 
 echo "[2/4] 正在自动寻找并设置最快的 APK 镜像源..."
-setup-apkrepos -1
+setup-apkrepos -cf
 
 echo "[3/4] 正在更新系统并安装必要组件..."
 apk update 
 apk upgrade 
-apk add nano curl iperf3 dos2unix zstd doas logrotate wget iputils-ping unzip htop fastfetch util-linux
+apk add nano curl iperf3 dos2unix zstd doas logrotate wget iputils-ping unzip htop fastfetch util-linux bash
 
 echo "[4/4] 正在初始化用户..."
 # 注意：setup-user 默认是交互式命令，运行到这里时会暂停并要求你输入新用户名和密码
