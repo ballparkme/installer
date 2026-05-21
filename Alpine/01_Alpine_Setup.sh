a407fc321bd7082a8a826df8f5b6834813804f4d
AllinAll <40621866+ballparkme@users.noreply.github.com>
2026-05-21 15:05:56 +0800

    Update 01_Alpine_Setup.sh

--- Alpine/01_Alpine_Setup.sh
+++ Alpine/01_Alpine_Setup.sh
@@ -14,7 +14,7 @@ setup-apkrepos -1
 echo "[3/4] 正在更新系统并安装必要组件..."
 apk update 
 apk upgrade 
-apk add nano curl iperf3 dos2unix zstd doas logrotate wget iputils-ping unzip htop fastfetch
+apk add nano curl iperf3 dos2unix zstd doas logrotate wget iputils-ping unzip htop fastfetch util-linux
 
 echo "[4/4] 正在初始化用户..."
 # 注意：setup-user 默认是交互式命令，运行到这里时会暂停并要求你输入新用户名和密码
